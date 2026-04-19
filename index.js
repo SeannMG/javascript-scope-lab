33082edaa86cd3cbdcff5fe52c46cccce8b5fded
Sean Macharia <Snmuanzi@gmail.com>
2026-04-19 12:56:29 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+// GLOBAL SCOPE
+const burgers = ["Hamburger", "Cheeseburger"]
+let featuredDrink = "Strawberry Milkshake"
+
+// FUNCTION SCOPE
+function addBurger() {
+    const newBurger  = "Flatburger"
+    burgers.push(newBurger)
+}
+
+// BLOCK SCOPE
+if(true) {
+    const anotherNewBurger = "Maple Bacon Burger"
+    burgers.push(anotherNewBurger)
+}
+
+function changeFeaturedDrink() {
+    featuredDrink = "The JavaShake"
+}
+
+module.exports = {
+    burgers,
+    featuredDrink,
+    addBurger,
+    changeFeaturedDrink
+}
